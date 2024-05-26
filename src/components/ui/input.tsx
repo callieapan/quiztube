c22aa10d613a10942ad39d57468410bf000b4f86
Michael Olie <mikeolie@Michaels-MacBook-Pro.local>
2024-05-25 17:17:04 -0700

    able to handle youtube url change

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -21,17 +21,24 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         setError('');
       }
     };
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setUrl(e.target.value);
+    };
     return (
-      <input
-        type={type}
-        className={cn(
-          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-          className,
-        )}
-        onBlur={handleBlur}
-        ref={ref}
-        {...props}
-      />
+      <>
+        <input
+          type={type}
+          onChange={handleOnChange}
+          className={cn(
+            'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+            className,
+          )}
+          onBlur={handleBlur}
+          ref={ref}
+          {...props}
+        />
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </>
     );
   },
 );
